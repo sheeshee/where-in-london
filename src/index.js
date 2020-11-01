10f652b40ba78530fc70669f75219e3a881818e6
Sam Sheehy <samuelsheehy95@gmail.com>
2020-11-01 14:17:45 +0000

    Remove unnecessary mapid div.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-     <div id="mapid"></div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
